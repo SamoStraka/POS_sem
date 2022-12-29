7bbb327a95b606089f21e48536658e1930818609
SamoStraka <samostraka@gmail.com>
2022-12-29 14:59:42 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/pos_sem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pos_sem.dir/main.c.o"
+  "pos_sem"
+  "pos_sem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pos_sem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/pos_sem.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for pos_sem.
+# This may be replaced when dependencies are built.
